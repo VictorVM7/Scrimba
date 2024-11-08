926dfb40076db4fcd451a389e5f9cb76c351fdaf
Victor Magalhães <vicmagalhaes20@gmail.com>
2024-11-08 14:10:32 -0300

    adding comment to destructuring props
    watched some classes and next is array.prototype.map()

--- airbnb/src/components/Card.tsx
+++ airbnb/src/components/Card.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import '../assets/css/Cards.css'
 
+
+// Destructuring Props is declare any parameter that has been passed in props object as it follows
 export default function Card(props){
     return (
         <main>
@@ -16,12 +18,43 @@ export default function Card(props){
                         alt='Star Icon'
                     />
                     <span>{props.rate}</span>
-                    <span className='gray'>(6) •</span>
+                    <span className='gray'>{props.review} •</span>
                     <span className='gray'>{props.country}</span>
                 </div>
                 <h2>{props.title}</h2>
-                <p><span>From ${props.price}</span> / person</p>
+                <p><span><b>From ${props.price}</b></span> / person</p>
             </div>
         </main>
     )
 }
+
+
+/*
+// Destructuring Props: Destructuring Props is declare any parameter that has been passed in props object as it follows.
+//                      The parameters name may be the same or you can use ":" to rename it.
+
+export default function Card({img, title, rate, country, price}){
+    return (
+        <main>
+            <div className="card">
+                <img
+                    src={img}
+                    className='card--image'
+                    alt='Image of Katie Zaferes'
+                />
+                <div className="card--stats">
+                    <img
+                        src="src\images\cards\star.png"
+                        alt='Star Icon'
+                    />
+                    <span>{rate}</span>
+                    <span className='gray'>(6) •</span>
+                    <span className='gray'>{country}</span>
+                </div>
+                <h2>{title}</h2>
+                <p><span>From ${price}</span> / person</p>
+            </div>
+        </main>
+    )
+}
+*/
--- airbnb/src/components/Cards.tsx
+++ airbnb/src/components/Cards.tsx
@@ -9,8 +9,9 @@ export default function Cards(){
                 img="src\images\cards\katie-zaferes.png"
                 title="Life Lessons with Katie Zaferes"
                 rate="5.0"
+                review= {6}
                 country="USA"
-                price="136"
+                price={136}
             />
         </main>
     )
