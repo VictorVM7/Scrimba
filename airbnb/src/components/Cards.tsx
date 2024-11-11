8fc529d5bfc245b963f8b16b8cef322726fa0b42
Victor Magalhães <vicmagalhaes20@gmail.com>
2024-11-11 14:14:55 -0300

    adding array.map and CardsData

--- airbnb/src/components/Cards.tsx
+++ airbnb/src/components/Cards.tsx
@@ -3,6 +3,7 @@ import '../assets/css/Card.css'
 import Card from './Card'
 
 export default function Cards(){
+
     return (
         <main>
             <Card 
---
+++ airbnb/src/data/CardsData.js
@@ -0,0 +1,10 @@
+export default [
+    {
+        img:"src\images\cards\katie-zaferes.png",
+        title:"Life Lessons with Katie Zaferes",
+        rate:"5.0",
+        review: 6,
+        country:"USA",
+        price:136,
+    }
+]
