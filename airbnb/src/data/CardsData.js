9e576ce8078c48f359bd1c04d4f8e0f15ce1e025
Victor Magalhães <vicmagalhaes20@gmail.com>
2024-11-12 14:04:00 -0300

    changings folders and trying implement data.js

--- airbnb/src/data/CardsData.js
+++ airbnb/src/data/CardsData.js
@@ -1,6 +1,8 @@
-export default [
+import React from 'react';
+
+export const CardsData = [
     {
-        img:"src\images\cards\katie-zaferes.png",
+        img:"src\\images\\cards\\katie-zaferes.png",
         title:"Life Lessons with Katie Zaferes",
         rate:"5.0",
         review: 6,
