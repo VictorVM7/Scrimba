3104bb776d2e2b27d2c42b303051fbe50fd15b47
Victor Magalhães <vicmagalhaes20@gmail.com>
2024-11-14 14:10:01 -0300

    adding new resources and variables

--- airbnb/src/data/CardsData.ts
+++ airbnb/src/data/CardsData.ts
@@ -18,7 +18,7 @@ export const CardsData: CardData[] = [
         title: "Life Lessons with Katie Zaferes",
         description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
         price: 136,
-        img: "public/images/cards/katie-zaferes.png",
+        img: "/images/cards/katie-zaferes.png",
         stats: {
             rating: 5.0,
             reviewCount: 6
@@ -31,7 +31,7 @@ export const CardsData: CardData[] = [
         title: "Learn Wedding Photography",
         description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
         price: 125,
-        img: "public/images/cards/wedding-photography.png",
+        img: "/images/cards/wedding-photography.png",
         stats: {
             rating: 5.0,
             reviewCount: 30
@@ -44,7 +44,7 @@ export const CardsData: CardData[] = [
         title: "Group Mountain Biking",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
-        img: "public/images/cards/mountain-bike.png",
+        img: "/images/cards/mountain-bike.png",
         stats: {
             rating: 4.8,
             reviewCount: 2
