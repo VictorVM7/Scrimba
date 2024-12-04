cd66c549f968316b6a59f3971100f1991c71a52c
Victor Magalhães <vicmagalhaes20@gmail.com>
2024-12-04 14:16:32 -0300

    adding eslint and header padding

--- chef-claude/src/components/Header.tsx
+++ chef-claude/src/components/Header.tsx
@@ -3,7 +3,7 @@ import ChefClaudeLogo from '/images/chef-claude-logo.png'
 export default function Header() {
     return (
         <>
-            <header>
+            <header className='p-2 shadow-md'>
                 <div className='justify-items-center'>
                     <img src={ChefClaudeLogo} alt={ChefClaudeLogo} />
                 </div>
