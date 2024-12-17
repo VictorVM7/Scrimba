4a53b47d33cc897a6bcdb539fd9f152423933f14
VictorVM7 <vicmagalhaes20@gmail.com>
2024-12-17 00:10:19 -0300

    adding list

--- chef-claude/src/components/Input.tsx
+++ chef-claude/src/components/Input.tsx
@@ -1,9 +1,17 @@
-export default function  InputIngredients () {
+import React, {ChangeEventHandler} from "react";
+
+interface InputIngredientsProps {
+    handleClick: React.MouseEventHandler<HTMLButtonElement>;
+    onChangeInput: ChangeEventHandler<HTMLInputElement>;
+    inputValue: string;
+}
+
+export default function  InputIngredients ({handleClick, inputValue, onChangeInput}: InputIngredientsProps) {
     return (
         <nav className={'my-10 w-screen justify-items-center align-middle align-items-center'}>
             <div className={'flex gap-4'}>
-                <input className={'w-96 h-10 p-4 border-2 rounded-md'} placeholder={'e.g oregano'}/>
-                <button className={'w-52 px-4 py-1 bg-black text-white rounded-md'}>
+                <input className={'w-96 h-10 p-4 border-2 rounded-md'} placeholder={'e.g oregano'} value={inputValue} onChange={onChangeInput}/>
+                <button className={'w-52 px-4 py-1 bg-black text-white rounded-md'} onClick={handleClick}>
                     <text className={'font-medium'}>
                         + Add ingredient
                     </text>
--- chef-claude/src/pages/Recipe.tsx
+++ chef-claude/src/pages/Recipe.tsx
@@ -1,10 +1,31 @@
 import Main from "../layout/Main.tsx";
 import InputIngredients from "../components/Input.tsx";
+import {useState} from "react";
 
 export default function Recipe(){
+    // State - Control state of variables
+    const [ingredients, setIngredients] = useState<string[]>([]);
+    const [inputValue, setInputValue] = useState<string>("");
+
+    // Event - OnClick add items
+    const addIngredient = () => {
+        setIngredients([...ingredients, inputValue]);
+        setInputValue("");
+    }
+
+    // Wrapper - Transforms the event into the string.
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value);
+    }
+
     return (
         <Main>
-            <InputIngredients />
+            <InputIngredients handleClick={addIngredient} inputValue={inputValue} onChangeInput={handleInputChange}/>
+            <ul>
+                {ingredients.map((ingredient, index) =>
+                    <li key={index}>{ingredient}</li>
+                )}
+            </ul>
         </Main>
     )
 }
