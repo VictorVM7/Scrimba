d314d603f15c02f039db876037a55d6b3e3b2f95
Victor Magalhães <vicmagalhaes20@gmail.com>
2024-12-18 14:04:01 -0300

    adding layout and toast

--- chef-claude/src/components/Input.tsx
+++ chef-claude/src/components/Input.tsx
@@ -8,10 +8,10 @@ interface InputIngredientsProps {
 
 export default function  InputIngredients ({handleClick, inputValue, onChangeInput}: InputIngredientsProps) {
     return (
-        <nav className={'my-10 w-screen justify-items-center align-middle align-items-center'}>
+        <nav className={'my-10 justify-items-center align-middle align-items-center'}>
             <div className={'flex gap-4'}>
                 <input className={'w-96 h-10 p-4 border-2 rounded-md'} placeholder={'e.g oregano'} value={inputValue} onChange={onChangeInput}/>
-                <button className={'w-52 px-4 py-1 bg-black text-white rounded-md'} onClick={handleClick}>
+                <button className={'px-4 py-1 bg-black text-white rounded-md'} onClick={handleClick}>
                     <text className={'font-medium'}>
                         + Add ingredient
                     </text>
